ee617e6bedfddb29935672f7fea432fc558c4ea3
Yves <yves.curel@gmail.com>
2024-03-14 16:52:41 +0100

    Update sailtrack.py

--- raspberryPi0W/python/sailtrack.py
+++ raspberryPi0W/python/sailtrack.py
@@ -12,9 +12,9 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('/home/pi/sailtrack/sailing-track-pkey.json')
+cred = credentials.Certificate('xxxx-pkey.json')
 # Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred, {'databaseURL': "https://sailing-track-default-rtdb.firebaseio.com"})
+firebase_admin.initialize_app(cred, {'databaseURL': "https://xxxx.firebaseio.com"})
 
 id=sys.argv[1]
 voyage=sys.argv[2]
